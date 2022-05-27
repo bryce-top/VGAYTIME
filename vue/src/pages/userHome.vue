9817b39e7f074c30683f24adb56824b01e335454
夜阑梦听雨 <1250575796@qq.com>
2022-05-27 19:54:43 +0800

    2022/5/27 19:54 用户个人信息修改实现

--- vue/src/pages/userHome.vue
+++ vue/src/pages/userHome.vue
@@ -16,7 +16,7 @@
             label-align-sm="right"
             label-for="nested-street"
           >
-            <b-form-input id="nested-street" v-model="username"></b-form-input>
+            <b-form-input id="nested-street" v-model="user.username"></b-form-input>
           </b-form-group>
 
           <b-form-group
@@ -25,7 +25,7 @@
             label-align-sm="right"
             label-for="nested-city"
           >
-            <b-form-input id="nested-city" v-model="account"></b-form-input>
+            <b-form-input id="nested-city" v-model="user.account"></b-form-input>
           </b-form-group>
 
           <b-form-group
@@ -34,7 +34,7 @@
             label-align-sm="right"
             label-for="nested-state"
           >
-            <b-form-input id="nested-state" v-model="email"></b-form-input>
+            <b-form-input id="nested-state" v-model="user.email"></b-form-input>
           </b-form-group>
 
           <b-form-group
@@ -43,19 +43,10 @@
             label-align-sm="right"
             label-for="nested-country"
           >
-            <b-form-input id="nested-country"></b-form-input>
+            <b-form-input id="nested-country" v-model="user.password" type="password"></b-form-input>
           </b-form-group>
 
-          <b-form-group
-            label-cols-sm="3"
-            label="Ship via:"
-            label-align-sm="right" class="mb-0"
-          >
-            <b-form-radio-group
-              class="pt-2"
-              :options="['Air', 'Courier', 'Mail']"
-            ></b-form-radio-group>
-          </b-form-group>
+          <b-button @click="changeInfo">提交</b-button>
         </b-form-group>
       </b-card>
     </div>
@@ -66,27 +57,52 @@
 import headbar from '@/pages/header';
 export default {
   name: "userHome",
+
   components:{
     headbar
   },
   data(){
     return{
-      username:'',
-      account:'',
-      email:''
+      user:{
+        username:'',
+        account:'',
+        email:'',
+        password:''
+      },
     }
   },
+
   created() {
     if (!this.$cookies.isKey('account')){
       this.$router.push("/login")
     }
     this.axios.post('/do/user/getUserinfo', "account="+this.$cookies.get('account')).then(res=>{
       if(res.data){
-        this.username=res.data.username;
-        this.account=res.data.account;
-        this.email=res.data.email;
+        this.user.username=res.data.username;
+        this.user.account=res.data.account;
+        this.user.email=res.data.email;
+        this.user.password=res.data.password;
+        // this.user=res.data;
       }
     })
+
+  },
+
+  methods:{
+
+    changeInfo(){
+
+      alert(this.user.account)
+      this.axios.post('/do/user/updateUser', this.user)
+        .then(res=> {
+          console.log(res);
+
+
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
   }
 }
 </script>
